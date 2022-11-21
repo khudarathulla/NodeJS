e6b767d15086c5d1070c03a494c06e5a7f6a00b3
Qudrat <qudratshaik786@gmail.com>
2022-11-22 00:33:33 +0530

    AA

--- index.js
+++ index.js
@@ -1,26 +1,82 @@
-const express = require('express')
-const app = express();
-const mysql = require('mysql2');
-const port = 3000
+const express = require("express");
+const cors = require("cors");
 
-const connection = mysql.createConnection({
+const app = express();
+const sql = require('mysql2/promise');
+const { createConnection } = require("mysql2");
+const PORT = process.env.PORT || 8080;
+const sqlConfig = {
     host: 'localhost',
     user: 'root',
-    database: 'mysqldb',
     password: 'Qudrat$haik@786',
-    port: 3306
-  });
+    database: 'mysqldb',
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// simple route
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World" });
+});
+
+app.get("/get-users", async (req, res) => {
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        const connection = await sql.createConnection(sqlConfig);
+        const result = await connection.execute(`SELECT * FROM employees;`)
+        const records=result[0];
+        res.status(200).send({ "Message": "Success", records })
+    } catch (err) {
+        res.status(500).send({ "Message": "Something went wrong" })
+    }
 })
 
-app.get('/employees', (req, res) => {
-    connection.query('SELECT * FROM employees', (err, result, fields) => {
-            res.status(200).send(result);
-        });
-  })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.put("/put-users", async (req,res) =>{
+    try {
+        const {sname, smarks, section, email, sroll} = req.body
+        const connection = await sql.createConnection(sqlConfig);
+        const result = await connection.execute(`UPDATE mydetails SET sname=?, smarks=?, section=?, email=? where sroll=?;`,[sname, smarks, section, email, sroll])
+        const records = result[0];
+        res.status(200).send({ "Message": "Success" , records })
+        
+    } catch (error) {
+        res.status(500).send({ "Message": "Something went wrong"})
+    }
 })
+
+
+app.post("/post-users", async (req,res) =>{
+    try {
+        const {sroll, sname, smarks, section, email} = req.body
+        const connection = await sql.createConnection(sqlConfig);
+        const result = await connection.execute(`INSERT INTO MYDETAILS VALUES(?,?,?,?,?)`,[sroll, sname, smarks, section, email]);
+        const records = result[0];
+        res.status(200).send({"Message":"Success", records})    
+    } catch (error) {
+        res.status(500).send({"Message": "Something went wrong"})
+    }
+})
+
+app.delete("/delete-users", async (req,res) =>{ 
+    try {
+        const {sroll} = req.body;
+        const connection = await sql.createConnection(sqlConfig);
+        const result = await connection.execute(`DELETE FROM MYDETAILS WHERE sroll=?`, [sroll]);
+        const records = result[0];
+        res.status(200).send ({"Message":"Success", records})
+    }catch (error) {
+        res.status(500).send({"MEssage": "Something went Wrong"})
+    }
+
+})
+
+// set port, listen for requests
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    // console.log("Server is running on port, ${PORT}");
+});
+
+
